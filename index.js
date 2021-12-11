9ca117421681a118e267a0ab37c24115a47938ef
Vijay Chandran <VijayC@softuradomain.com>
2021-12-11 10:38:00 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express=require("express");
+var passport = require('passport');
+var users=require('./routes/userroutes')
+var department=require('./orm/mysqlroutes')
+var app=express();
+const cors=require("cors")
+app.use(passport.initialize());
+app.use(cors())
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use("/users",users)
+
+app.listen("8000",function(){
+    console.log("Server running on port 8000")
+})
